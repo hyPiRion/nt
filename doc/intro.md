786cdb03cd4888219146fee3546594691737026d
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-04-02 19:24:50 +0200

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nt
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/com/hypirion/io/nt_test.clj
@@ -0,0 +1,7 @@
+(ns com.hypirion.io.nt-test
+  (:use clojure.test
+        com.hypirion.io.nt))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
