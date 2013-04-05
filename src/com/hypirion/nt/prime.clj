73c18166e9fca85795ed40e8cb6ce561edc52f94
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-04-05 02:30:26 +0200

    Implement fast euler-totient.

--- src/com/hypirion/nt/prime.clj
+++ src/com/hypirion/nt/prime.clj
@@ -1,5 +1,6 @@
 (ns com.hypirion.nt.prime
-  (require [com.hypirion.prime :as p]))
+  (:require [com.hypirion.prime :as p]
+            [clojure.math.numeric-tower :refer [expt]]))
 
 (defn divisor-count [n]
   (->> n p/factorize frequencies keys (map inc) (reduce *)))
@@ -9,3 +10,24 @@
 
 (defn radical [n]
   (reduce * (unique-prime-factors n)))
+
+(defn- totient-pow [p k]
+    (* (expt p (- k 1)) (- p 1)))
+
+;; TODO: Make this completely tail recursive.
+(defn- acc-tot [d primes]
+  (cond (= 1 d) 1
+        (prime? d) (- d 1)
+        :else (loop [[p & r] primes]
+                (if (zero? (rem d p))
+                  (loop [k 1
+                         d (quot d p)]
+                    (if (zero? (rem d p))
+                      (recur (inc k)
+                             (quot d p))
+                      (* (acc-tot d r)
+                         (totient-pow p k))))
+                  (recur r)))))
+
+(defn euler-totient [d]
+  (acc-tot d p/primes))
