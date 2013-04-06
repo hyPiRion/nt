61ea3b9d6cc5a0118872a2ec1dba6ca206cea4a3
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-04-06 14:44:30 +0200

    Bump prime dependency.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [com.hypirion/primes "0.2.0"]])
+                 [com.hypirion/primes "0.2.1"]])
