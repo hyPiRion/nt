88affc78c5cba300c0f7c24691e2aa2acf2ed9bf
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-04-08 02:06:57 +0200

    Allow for arbitrary radixes in digit manipulation.

--- src/com/hypirion/nt/digits.clj
+++ src/com/hypirion/nt/digits.clj
@@ -1,32 +1,35 @@
 (ns com.hypirion.nt.digits)
 
 (defn num->digits
-  "Converts the number n into a list of digits in base 10."
-  [n]
-  (loop [m n
-         acc nil]
-    (if (zero? m)
-      acc
-      (recur (quot m 10)
-             (cons (rem m 10) acc)))))
+  "Converts the number n into a list of digits in radix r. r defaults to 10."
+  ([n] (num->digits n 10))
+  ([n r]
+     (loop [m n
+            acc ()]
+       (if (zero? m)
+         acc
+         (recur (quot m r)
+                (conj acc (rem m r)))))))
 
 (defn digits->num
-  "Converts a sequence of digits in base 10 to an integer."
-  [coll]
-  (reduce
-   (fn [a b] (+ (* 10 a) b))
-   0
-   coll))
+  "Converts a sequence of digits in radix r to an integer. r defaults to 10."
+  ([coll] (digits->num coll 10))
+  ([coll r]
+     (reduce
+      (fn [a b] (+ (* r a) b))
+      0
+      coll)))
 
 (defn digital-sum
-  "Returns the digital sum of the integer (in base 10)."
-  [n]
-  (loop [m n
-         acc 0]
-    (if (zero? m)
-      acc
-      (recur (quot m 10)
-             (+ acc (rem m 10))))))
+  "Returns the digital sum of the integer in radix r. r defaults to 10."
+  ([n] (digital-sum n 10))
+  ([n r]
+      (loop [m n
+             acc 0]
+        (if (zero? m)
+          acc
+          (recur (quot m r)
+                 (+ acc (rem m r)))))))
 
 (defn bit-count
   "Counts the amount of bits in v."
