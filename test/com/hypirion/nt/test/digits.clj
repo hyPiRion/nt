e1d441455474a664b920221d069a99da6fb95b2b
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-12-31 16:28:45 +0100

    Check num-digits check with radix.

--- test/com/hypirion/nt/test/digits.clj
+++ test/com/hypirion/nt/test/digits.clj
@@ -7,12 +7,34 @@
             [clojure.test :refer :all]))
 
 (defspec num-digits-conversion
-  1000
+  10000
   (prop/for-all [n gen/int]
     (= n (digits->num (num->digits n)))))
 
+(defspec num-digits-conversion-w-radix
+  10000
+  (prop/for-all [r (gen/choose 1 1000000) n gen/int]
+    (= n (digits->num (num->digits n r) r))))
+
 (defspec digits-num-conversion
-  1000
+  10000
   (prop/for-all [digits (gen/vector (gen/choose 1 9) 1 18)]
     (= digits (num->digits (digits->num digits)))))
 
+(defn largest-vector-size [radix]
+  (loop [n Long/MAX_VALUE
+         iters 0]
+    (if (zero? n)
+      (dec iters)
+      (recur (quot n radix)
+             (inc iters)))))
+
+(defspec digits-num-conversion-w-radix
+  10000
+  (prop/for-all [[r digits]
+                 (gen/bind (gen/choose 1 1000000)
+                           (fn [r]
+                             (gen/tuple (gen/return r)
+                                        (gen/vector (gen/choose 1 r)
+                                                    1 (largest-vector-size r)))))]
+    (= digits (num->digits (digits->num digits r) r))))
